a5c5c90e8685d809414c51e30469008037b31152
Jon-LaFlamme <jonlaflamme7@gmail.com>
2021-08-01 00:18:15 -0500

    update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,12 +1,8 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
-from flask_jsglue import JSGlue
 
-csrf = CSRFProtect()
 app = Flask(__name__)
-jsglue = JSGlue(app)
 app.config['SECRET_KEY'] = 'SECRET'
 app.config['WTF_CSRF_ENABLED'] = False
-csrf.init_app(app)
+
 
 import flaskr.routes
